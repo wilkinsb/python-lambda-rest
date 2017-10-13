0662d200a241e286661ef8bacd440649cb8f3f3a
wilkinsb <bwilkins@sciscogenetics.com>
2017-10-13 13:13:26 -0700

    add httpauth methods, flask_restful package; create Worker resource

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
+import os
+
 from flask import Flask
+from flask_httpauth import HTTPBasicAuth
+from flask_restful import Resource, Api, reqparse
+from flask_restful import abort as fr_abort
 
 app = Flask(__name__)
 
+auth = HTTPBasicAuth()
 
-@app.route('/')
-def index():
-    return "Hello, world!", 200
+
+@auth.verify_password
+def verify_password(username, password):
+    """
+    Authentication method.
+    """
+    return username == os.getenv('LOGIN_USERNAME') and \
+           password == os.getenv('LOGIN_PASSWORD')
+
+
+class Worker(Resource):
+    """
+    The DB worker resource (/worker endpoint)
+    """
+
+    def get(self):
+        """
+        GET method for /worker resource
+        """
+        pass
+
+    @auth.login_required
+    def post(self):
+        """
+        POST method for /worker resource
+        """
+        pass
 
 
 # We only need this for local development.
